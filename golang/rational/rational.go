4cb9007ff81ee8bc1578eb5169e85410f233a08f
Jared Davis <jared@haiq.us>
2014-05-22 12:08:22 -0500

    [GO] [CS] ran `go fmt rational.go`
    modified:   golang/rational/rational.go

--- golang/rational/rational.go
+++ golang/rational/rational.go
@@ -1,38 +1,38 @@
 package rational
 
-import(
-  "fmt"
+import (
+	"fmt"
 )
 
 type Rational struct {
-  Numer int
-  Denom int
+	Numer int
+	Denom int
 }
 
 func Add(this Rational, that Rational) Rational {
-  return Rational{
-    (this.Numer * that.Denom) + (this.Denom * that.Numer),
-    this.Denom * that.Denom }
+	return Rational{
+		(this.Numer * that.Denom) + (this.Denom * that.Numer),
+		this.Denom * that.Denom}
 }
 
 func Mult(this Rational, that Rational) Rational {
-  return Rational{
-    this.Numer * that.Numer,
-    this.Denom * that.Denom }
+	return Rational{
+		this.Numer * that.Numer,
+		this.Denom * that.Denom}
 }
 
 func LessThan(this Rational, that Rational) bool {
-  return this.Numer * that.Denom < that.Numer * this.Denom
+	return this.Numer*that.Denom < that.Numer*this.Denom
 }
 
 func Max(a Rational, b Rational) Rational {
-  if (LessThan( a, b )) {
-    return b
-  } else {
-    return a
-  }
+	if LessThan(a, b) {
+		return b
+	} else {
+		return a
+	}
 }
 
 func (some Rational) Print() {
-  fmt.Printf("Rational: %d/%d\n", some.Numer, some.Denom)
+	fmt.Printf("Rational: %d/%d\n", some.Numer, some.Denom)
 }
